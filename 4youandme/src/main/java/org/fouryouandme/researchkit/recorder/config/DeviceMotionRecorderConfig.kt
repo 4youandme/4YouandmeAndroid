3e78c7073ec4a8de4c6ddd17199b4a73c99ee7d9
Giacomo Parisi <giacomo@balzo.eu>
2020-09-04 14:38:32 +0200

    Fix device motion config

--- 4youandme/src/main/java/org/fouryouandme/researchkit/recorder/config/DeviceMotionRecorderConfig.kt
+++ 4youandme/src/main/java/org/fouryouandme/researchkit/recorder/config/DeviceMotionRecorderConfig.kt
@@ -3,7 +3,7 @@ package org.fouryouandme.researchkit.recorder.config
 import com.squareup.moshi.Moshi
 import org.fouryouandme.researchkit.recorder.Recorder
 import org.fouryouandme.researchkit.recorder.RecorderConfig
-import org.fouryouandme.researchkit.recorder.sensor.AccelerometerRecorder
+import org.fouryouandme.researchkit.recorder.sensor.DeviceMotionRecorder
 import org.fouryouandme.researchkit.step.Step
 import java.io.File
 
@@ -17,6 +17,6 @@ class DeviceMotionRecorderConfig(
 ) : RecorderConfig("device_motion") {
 
     override fun recorderForStep(step: Step, outputDirectory: File): Recorder =
-        AccelerometerRecorder(frequency, identifier, step, outputDirectory, moshi)
+        DeviceMotionRecorder(frequency, identifier, step, outputDirectory, moshi)
 
 }
