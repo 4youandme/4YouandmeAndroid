a61e53e128bd15570c2c5076908ff892b5e060ab
Giacomo Parisi <giacomo@balzo.eu>
2020-10-03 12:10:11 +0200

    Add page view html body parsing

--- 4youandme/src/main/java/org/fouryouandme/core/view/page/PageView.kt
+++ 4youandme/src/main/java/org/fouryouandme/core/view/page/PageView.kt
@@ -21,6 +21,7 @@ import org.fouryouandme.core.entity.configuration.HEXGradient
 import org.fouryouandme.core.entity.configuration.button.button
 import org.fouryouandme.core.entity.page.Page
 import org.fouryouandme.core.ext.dpToPx
+import org.fouryouandme.core.ext.html.setHtmlText
 import org.fouryouandme.core.ext.imageConfiguration
 import org.fouryouandme.core.view.page.EPageType.*
 
@@ -59,7 +60,7 @@ class PageView(context: Context, attrs: AttributeSet?) : FrameLayout(context, at
         title.text = page.title
         title.setTextColor(configuration.theme.primaryTextColor.color())
 
-        description.text = page.body
+        description.setHtmlText(page.body, true)
         description.setTextColor(configuration.theme.primaryTextColor.color())
 
         description.gravity =
