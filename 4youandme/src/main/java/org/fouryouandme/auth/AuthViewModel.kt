5c15bfb0ba8abf260945a8b145f32afc89d6a261
Giacomo Parisi <giacomo@balzo.eu>
2020-10-08 10:51:55 +0200

    Refactor sign up later

--- 4youandme/src/main/java/org/fouryouandme/auth/AuthViewModel.kt
+++ 4youandme/src/main/java/org/fouryouandme/auth/AuthViewModel.kt
@@ -5,6 +5,20 @@ import org.fouryouandme.core.arch.android.BaseViewModel
 import org.fouryouandme.core.arch.android.Empty
 import org.fouryouandme.core.arch.deps.Runtime
 import org.fouryouandme.core.arch.navigation.Navigator
+import org.fouryouandme.core.arch.navigation.RootNavController
 
 class AuthViewModel(navigator: Navigator, runtime: Runtime<ForIO>) :
-    BaseViewModel<ForIO, Empty, Empty, Empty, Empty>(Empty, navigator, runtime)
+    BaseViewModel<ForIO, Empty, Empty, Empty, Empty>(Empty, navigator, runtime) {
+
+
+    /* --- navigation --- */
+
+    suspend fun back(
+        authNavController: AuthNavController,
+        rootNavController: RootNavController
+    ): Unit {
+        if (navigator.back(authNavController).not())
+            navigator.back(rootNavController)
+    }
+
+}
--- 4youandme/src/main/java/org/fouryouandme/auth/signup/later/SignUpLaterViewModel.kt
+++ 4youandme/src/main/java/org/fouryouandme/auth/signup/later/SignUpLaterViewModel.kt
@@ -1,47 +1,18 @@
 package org.fouryouandme.auth.signup.later
 
-import androidx.navigation.NavController
-import arrow.core.toOption
 import arrow.fx.ForIO
-import org.fouryouandme.auth.signup.info.SignUpInfoToSignUpLater
 import org.fouryouandme.core.arch.android.BaseViewModel
+import org.fouryouandme.core.arch.android.Empty
 import org.fouryouandme.core.arch.deps.Runtime
 import org.fouryouandme.core.arch.navigation.Navigator
-import org.fouryouandme.core.cases.CachePolicy
-import org.fouryouandme.core.cases.configuration.ConfigurationUseCase
-import org.fouryouandme.core.ext.unsafeRunAsync
 
 class SignUpLaterViewModel(
     navigator: Navigator,
     runtime: Runtime<ForIO>
 ) : BaseViewModel<
         ForIO,
-        SignUpLaterState,
-        SignUpLaterStateUpdate,
-        SignUpLaterError,
-        SignUpLaterError>
-    (SignUpLaterState(), navigator, runtime) {
-
-    fun initialize(): Unit =
-        runtime.fx.concurrent {
-
-            val configuration =
-                !ConfigurationUseCase.getConfiguration(runtime, CachePolicy.MemoryFirst)
-
-            !configuration.fold(
-                { setError(it, SignUpLaterError.Initialization) },
-                {
-                    setState(
-                        state().copy(configuration = it.toOption()),
-                        SignUpLaterStateUpdate.Initialization(it)
-                    )
-                }
-            )
-
-        }.unsafeRunAsync()
-
-    /* --- navigation --- */
-
-    fun back(navController: NavController): Unit =
-        navigator.back(runtime, navController).unsafeRunAsync()
-}
+        Empty,
+        Empty,
+        Empty,
+        Empty>
+    (Empty, navigator, runtime)
