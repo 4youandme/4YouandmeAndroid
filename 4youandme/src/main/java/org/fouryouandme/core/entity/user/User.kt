1384ffb34829f2e0e8d3860b617a84189a5818bc
Giacomo Parisi <giacomo@balzo.eu>
2020-10-09 11:41:15 +0200

    Add user custom data entities objects

--- 4youandme/src/main/java/org/fouryouandme/core/entity/user/User.kt
+++ 4youandme/src/main/java/org/fouryouandme/core/entity/user/User.kt
@@ -7,5 +7,26 @@ data class User(
     val daysInStudy: Int,
     val identities: List<String>,
     val onBoardingCompleted: Boolean,
-    val token: String
+    val token: String,
+    val customData: List<UserCustomData>
 )
+
+data class UserCustomData(
+    val identifier: String,
+    val value: String?,
+    val name: String,
+    val type: UserCustomDataType,
+)
+
+data class UserCustomDataItem(
+    val identifier: String,
+    val value: String
+)
+
+sealed class UserCustomDataType {
+
+    object String : UserCustomDataType()
+    object Date : UserCustomDataType()
+    data class Items(val items: List<UserCustomDataItem>)
+
+}
