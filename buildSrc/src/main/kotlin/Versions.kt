f8518f8961dd7dd3848b44467bb59697a53164f2
Giacomo Parisi <giacomo@balzo.eu>
2020-10-09 10:32:10 +0200

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -15,17 +15,19 @@ object Versions {
 
     const val com_squareup_retrofit2: String = "2.9.0"
 
-    const val org_jetbrains_kotlin: String = "1.4.0" // available: "1.4.10"
+    const val org_jetbrains_kotlin: String = "1.4.10"
 
     const val androidx_navigation: String = "2.2.2" // available: "2.3.0"
 
     const val androidx_lifecycle: String = "2.2.0"
 
+    const val com_squareup_moshi: String = "1.11.0"
+
     const val moe_banana: String = "3.5.0"
 
     const val android_arch_lifecycle_extensions: String = "1.1.1"
 
-    const val com_android_tools_build_gradle: String = "4.0.1"
+    const val com_android_tools_build_gradle: String = "4.0.2"
 
     const val androidx_test_ext_junit: String = "1.1.2"
 
@@ -37,23 +39,21 @@ object Versions {
 
     const val arrow_fx_coroutines: String = "0.11.0"
 
-    const val logging_interceptor: String = "4.8.1" // available: "4.9.0"
+    const val logging_interceptor: String = "4.9.0"
 
     const val recycler_droid_core: String = "1.8.0-beta06"
 
-    const val firebase_analytics: String = "17.5.0"
+    const val firebase_analytics: String = "17.6.0"
 
-    const val constraintlayout: String = "2.0.1"
+    const val constraintlayout: String = "2.0.2"
 
-    const val google_services: String = "4.3.3"
+    const val google_services: String = "4.3.4"
 
     const val security_crypto: String = "1.0.0-rc01"
 
     const val camera_camera2: String = "1.0.0-beta08"
 
-    const val moshi_adapters: String = "1.8.0" // available: "1.10.0"
-
-    const val mpandroidchart: String = "v3.1.0" // available: "3.1.0"
+    const val mpandroidchart: String = "3.1.0"
 
     const val espresso_core: String = "3.3.0"
 
@@ -65,13 +65,11 @@ object Versions {
 
     const val fragment_ktx: String = "1.3.0-alpha06"
 
-    const val moshi_kotlin: String = "1.10.0"
-
     const val camera_core: String = "1.0.0-beta08"
 
     const val camera_view: String = "1.0.0-alpha15"
 
-    const val lint_gradle: String = "27.0.1"
+    const val lint_gradle: String = "27.0.2"
 
     const val threetenabp: String = "1.2.4"
 
@@ -83,26 +81,26 @@ object Versions {
 
     const val appcompat: String = "1.2.0"
 
-    const val isoparser: String = "1.1.9" // available: "1.1.22"
+    const val isoparser: String = "1.1.22"
 
     const val arrow_fx: String = "0.11.0"
 
-    const val core_ktx: String = "1.3.1"
+    const val core_ktx: String = "1.3.2"
 
     const val dexter: String = "6.2.1"
 
-    const val lottie: String = "3.4.2"
+    const val lottie: String = "3.4.4"
 
     const val timber: String = "4.7.1"
 
     const val toasty: String = "1.5.0"
 
-    const val aapt2: String = "4.0.1-6197926"
+    const val aapt2: String = "4.0.2-6197926"
 
     const val ccp: String = "2.4.1"
 
     /**
-     * Current version: "6.5"
+     * Current version: "6.6.1"
      * See issue 19: How to update Gradle itself?
      * https://github.com/jmfayard/buildSrcVersions/issues/19
      */
