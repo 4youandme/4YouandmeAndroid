4f341f099199387e27670433872e29963eca7f06
Giacomo Parisi <giacomo@balzo.eu>
2020-10-23 14:50:29 +0200

    Add example events comment
    Signed-off-by: Giacomo Parisi <giacomo@balzo.eu>

--- 4youandme/src/main/java/org/fouryouandme/core/cases/analytics/AnalyticsEvent.kt
+++ 4youandme/src/main/java/org/fouryouandme/core/cases/analytics/AnalyticsEvent.kt
@@ -7,4 +7,14 @@ sealed class AnalyticsEvent(val eventName: String) {
 
     open fun firebaseBundle(): Bundle = bundleOf()
 
+
+    /*data class ExampleEventWithParams(val id: String): AnalyticsEvent("example_name") {
+
+        override fun firebaseBundle(): Bundle =
+            Bundle().also { it.putString("id", id) }
+
+    }
+
+    object ExampleEvent: AnalyticsEvent("example_name_2")*/
+
 }
