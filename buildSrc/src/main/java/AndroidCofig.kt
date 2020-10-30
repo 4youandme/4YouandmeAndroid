68537b1c0e0485deadc6b1a40c4c446ae0bc1253
Giacomo Parisi <giacomo@balzo.eu>
2020-10-30 16:31:02 +0100

    Release 0.1.6

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 7
-    const val version_name = "0.1.5"
+    const val version_code = 8
+    const val version_name = "0.1.6"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 }
