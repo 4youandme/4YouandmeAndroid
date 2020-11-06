9e6717751d31b5ac3fec5f08f47d1d35a84224eb
Giacomo Parisi <giacomo@balzo.eu>
2020-11-06 16:18:30 +0100

    Release 0.1.10

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 10
-    const val version_name = "0.1.9"
+    const val version_code = 11
+    const val version_name = "0.1.10"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 }
