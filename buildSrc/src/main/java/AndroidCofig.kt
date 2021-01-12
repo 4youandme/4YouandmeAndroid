219a4263aceac6ef54d3fc48eec93d5c107e4e41
Iacopo Checcacci <iacopo@balzo.eu>
2021-01-12 15:28:27 +0100

    Release 0.1.18

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 18
-    const val version_name = "0.1.17"
+    const val version_code = 19
+    const val version_name = "0.1.18"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 }
