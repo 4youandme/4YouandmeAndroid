7ca93afc8d743aca38c439d0d66d67b2a56634dc
Giacomo Parisi <giacomo@balzo.eu>
2021-03-05 09:51:44 +0100

    Fix onboarding completed api

--- foryouandme/src/main/java/com/foryouandme/data/repository/consent/user/network/request/CompleteUserConsentRequest.kt
+++ foryouandme/src/main/java/com/foryouandme/data/repository/consent/user/network/request/CompleteUserConsentRequest.kt
@@ -5,14 +5,16 @@ import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 
 data class CompleteUserConsentRequest(
-    @Json(name = "on_boarding_completed_at") val onBoardingCompletedAt: String
+    @Json(name = "on_boarding_completed_at") val onBoardingCompletedAt: String,
+    @Json(name = "agree") val agree: Boolean
 ) {
 
     companion object {
 
         fun build(): CompleteUserConsentRequest =
             CompleteUserConsentRequest(
-                LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                true
             )
 
     }
