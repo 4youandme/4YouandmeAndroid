0f0da178688e0af487869c859af48008d7638363
Giacomo Parisi <giacomo@balzo.eu>
2021-03-11 12:03:00 +0100

    Fix OnboardingStep ids

--- foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/consent/ConsentStep.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/consent/ConsentStep.kt
@@ -2,4 +2,4 @@ package com.foryouandme.ui.auth.onboarding.step.consent
 
 import com.foryouandme.ui.auth.onboarding.step.OnboardingStep
 
-object ConsentStep : OnboardingStep("consent-group", { ConsentFragment.build(false) })
+object ConsentStep : OnboardingStep("consent_group", { ConsentFragment.build(false) })
--- foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/consent/OptInStep.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/consent/OptInStep.kt
@@ -2,4 +2,4 @@ package com.foryouandme.ui.auth.onboarding.step.consent
 
 import com.foryouandme.ui.auth.onboarding.step.OnboardingStep
 
-object OptInStep : OnboardingStep("opt-ins", { ConsentFragment.build(true) })
+object OptInStep : OnboardingStep("opt_in", { ConsentFragment.build(true) })
