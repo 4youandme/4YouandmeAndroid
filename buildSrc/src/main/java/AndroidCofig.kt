3b5e83c734fd52e3afb7ec7be6c40fd4f5c46658
Giacomo Parisi <giacomo@balzo.eu>
2021-03-23 16:00:50 +0100

    Release 0.1.34

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 33
-    const val version_name = "0.1.33"
+    const val version_code = 34
+    const val version_name = "0.1.34"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
