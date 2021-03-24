02e3e8341ae66f8b256dc3ecf2394f2cba5dc20e
Iacopo Checcacci <iacopo@balzo.eu>
2021-03-24 11:47:18 +0100

    FYAM-721 date format on rewardable feed tiles

--- foryouandme/src/main/java/com/foryouandme/core/data/api/common/response/meta/MetaResponse.kt
+++ foryouandme/src/main/java/com/foryouandme/core/data/api/common/response/meta/MetaResponse.kt
@@ -1,9 +1,13 @@
 package com.foryouandme.core.data.api.common.response.meta
 
+import com.foryouandme.core.ext.catchToNull
 import com.squareup.moshi.Json
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import java.util.*
 
 data class MetaResponse(
-    @Json(name = "resolved_templates") val resolvedTemplates: List<ResolvedTemplateResponse>? = null
+        @Json(name = "resolved_templates") val resolvedTemplates: List<ResolvedTemplateResponse>? = null
 ) {
 
     fun applyMeta(source: String): String {
@@ -12,9 +16,14 @@ data class MetaResponse(
 
         resolvedTemplates?.forEach {
 
-            if (it.resolved != null && it.variable != null)
-                replacedSource =
+            if (it.resolved != null && it.variable != null) {
+                val date = getDate(it.resolved)
+                replacedSource = if (date != null) {
+                    val formattedDate = date.format(DateTimeFormatter.ofPattern("MMM dd")).capitalize(Locale.getDefault())
+                    replacedSource.replace("{{${it.variable}}}", formattedDate)
+                } else
                     replacedSource.replace("{{${it.variable}}}", it.resolved)
+            }
 
         }
 
@@ -22,9 +31,13 @@ data class MetaResponse(
 
     }
 
+    private fun getDate(dateStr: String): LocalDate? =
+            catchToNull {
+                LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+            }
 }
 
 data class ResolvedTemplateResponse(
-    @Json(name = "variable") val variable: String? = null,
-    @Json(name = "resolved") val resolved: String? = null
+        @Json(name = "variable") val variable: String? = null,
+        @Json(name = "resolved") val resolved: String? = null
 )
