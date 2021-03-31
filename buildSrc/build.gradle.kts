b368d9cdd8eec7d6946ccb0836edfd00c77dc057
Giacomo Parisi <giacomo@balzo.eu>
2021-03-31 17:38:34 +0200

    Move maven publication to foryouandme module build.gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,28 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-buildscript {
-
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
-        classpath("com.android.tools.build:gradle:7.0.0-alpha12")
-    }
-
-}
-
 repositories {
-    google()
     jcenter()
-}
-
-dependencies {
-
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
-    // Android gradle plugin will allow us to access Android specific features
-    implementation("com.android.tools.build:gradle:7.0.0-alpha12")
-
 }
