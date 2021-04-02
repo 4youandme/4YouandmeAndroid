f8c0b93819b9f8e4fc869d3ca5478ba93c8727b5
Giacomo Parisi <giacomo@balzo.eu>
2021-04-02 11:26:44 +0200

    Fix sensor step timer

--- foryouandme/src/main/java/com/foryouandme/researchkit/step/sensor/SensorStepFragment.kt
+++ foryouandme/src/main/java/com/foryouandme/researchkit/step/sensor/SensorStepFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.foryouandme.R
 import com.foryouandme.core.arch.flow.observeIn
 import com.foryouandme.core.arch.flow.unwrapEvent
 import com.foryouandme.core.ext.infoToast
+import com.foryouandme.core.ext.isTerminated
 import com.foryouandme.core.ext.launchSafe
 import com.foryouandme.core.ext.startCoroutineAsync
 import com.foryouandme.databinding.StepSensorBinding
@@ -17,11 +19,15 @@ import com.foryouandme.researchkit.recorder.*
 import com.foryouandme.researchkit.step.StepFragment
 import com.foryouandme.ui.tasks.TaskStateUpdate
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.onEach
 import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class SensorStepFragment : StepFragment(R.layout.step_sensor) {
+
+    private var timerJob: Job? = null
 
     private val serviceConnection: RecorderServiceConnection =
         RecorderServiceConnection(
@@ -114,37 +120,68 @@ class SensorStepFragment : StepFragment(R.layout.step_sensor) {
 
         val viewBinding = binding
 
-        viewBinding?.root?.setBackgroundColor(step.backgroundColor)
+        if(viewBinding != null) {
 
-        if (step.image != null) {
+            viewBinding.root.setBackgroundColor(step.backgroundColor)
 
-            viewBinding?.image?.visibility = View.VISIBLE
-            val lp = viewBinding?.image?.layoutParams
-            val displayMetrics = DisplayMetrics()
+            if (step.image != null) {
 
-            requireActivity().windowManager
-                .defaultDisplay
-                .getMetrics(displayMetrics)
+                viewBinding.image.visibility = View.VISIBLE
+                val lp = viewBinding.image.layoutParams
+                val displayMetrics = DisplayMetrics()
 
-            val height = displayMetrics.heightPixels
-            lp?.height = (height * 0.4).roundToInt()
-            viewBinding?.image?.layoutParams = lp
-            viewBinding?.image?.setImageResource(step.image)
-            viewBinding?.image?.setBackgroundColor(
-                Color.argb(255, 227, 227, 227)
+                requireActivity().windowManager
+                    .defaultDisplay
+                    .getMetrics(displayMetrics)
+
+                val height = displayMetrics.heightPixels
+                lp.height = (height * 0.4).roundToInt()
+                viewBinding.image.layoutParams = lp
+                viewBinding.image.setImageResource(step.image)
+                viewBinding.image.setBackgroundColor(
+                    Color.argb(255, 227, 227, 227)
+                )
+
+            } else viewBinding.image.visibility = View.GONE
+
+            viewBinding.title.text = step.title(requireContext())
+            viewBinding.title.setTextColor(step.titleColor)
+
+            viewBinding.description.text = step.description(requireContext())
+            viewBinding.description.setTextColor(step.descriptionColor)
+
+            RecorderService.start(requireContext().applicationContext, serviceConnection)
+
+            viewBinding.timer.isVisible = step.timerSeconds != null
+            viewBinding.timer.setTextColor(step.descriptionColor)
+
+            if (
+                (timerJob == null || timerJob.isTerminated) &&
+                step.timerSeconds != null &&
+                step.timerSeconds > 0
             )
+                timerJob = lifecycleScope.launchSafe {
 
-        } else viewBinding?.image?.visibility = View.GONE
+                    var seconds = step.timerSeconds
 
-        viewBinding?.title?.text = step.title(requireContext())
-        viewBinding?.title?.setTextColor(step.titleColor)
+                    while (seconds > 0) {
 
-        viewBinding?.description?.text = step.description(requireContext())
-        viewBinding?.description?.setTextColor(step.descriptionColor)
+                        val minuteValue = (seconds / 60).toString().formatTimeWithZero()
+                        val secondsValue = (seconds % 60).toString().formatTimeWithZero()
 
-        RecorderService.start(requireContext().applicationContext, serviceConnection)
+                        val timer = "$minuteValue:$secondsValue"
+                        binding?.timer?.text = timer
+                        seconds--
+                        delay(1000)
 
+                    }
+
+                }
+        }
     }
+
+    private fun String.formatTimeWithZero(): String =
+        if (length == 1) "0$this" else this
 
     override fun onDestroy() {
 
