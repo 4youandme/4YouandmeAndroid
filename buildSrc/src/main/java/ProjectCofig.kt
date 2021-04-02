b854fc6b46506bd962a1fe4b253859899fbb4b70
Giacomo Parisi <giacomo@balzo.eu>
2021-04-02 15:51:42 +0200

    Release 0.1.40

--- buildSrc/src/main/java/ProjectCofig.kt
+++ buildSrc/src/main/java/ProjectCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 39
-    const val version_name = "0.1.39"
+    const val version_code = 40
+    const val version_name = "0.1.40"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
