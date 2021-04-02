a54deee575e22214a61688c5e559b355d45dd5fe
Giacomo Parisi <giacomo@balzo.eu>
2021-04-02 10:39:38 +0200

    Release 0.1.38

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 37
-    const val version_name = "0.1.37"
+    const val version_code = 38
+    const val version_name = "0.1.38"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
