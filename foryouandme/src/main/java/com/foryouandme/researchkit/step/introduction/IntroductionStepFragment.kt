5b1b6d2ed5534773148163cb845b1fc45cab3025
Giacomo Parisi <giacomo@balzo.eu>
2021-04-22 16:37:21 +0200

    Remove hole intro fragment code

--- foryouandme/src/main/java/com/foryouandme/researchkit/step/introduction/IntroductionStepFragment.kt
+++ foryouandme/src/main/java/com/foryouandme/researchkit/step/introduction/IntroductionStepFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
 import com.foryouandme.researchkit.step.StepFragment
-import com.foryouandme.researchkit.step.holepeg.HolePeg
 import com.foryouandme.researchkit.step.introduction.compose.Introduction
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,38 +31,6 @@ class IntroductionStepFragment : StepFragment() {
 
         val step = taskViewModel.getStepByIndexAs<IntroductionStep>(indexArg())
         viewModel.execute(IntroductionAction.SetStep(step))
-
-    }
-
-    private fun applyData(
-        step: IntroductionStep
-    ) {
-
-        /*root.setBackgroundColor(step.backgroundColor)
-
-        val lp = image.layoutParams
-        val displayMetrics = DisplayMetrics()
-
-        requireActivity().windowManager
-            .defaultDisplay
-            .getMetrics(displayMetrics)
-
-        val height = displayMetrics.heightPixels
-        lp.height = (height * 0.4).roundToInt()
-        image.layoutParams = lp
-        image.setImageResource(step.image)
-        image.setBackgroundColor(Color.argb(255, 227, 227, 227))
-
-        title.text = step.title(requireContext())
-        title.setTextColor(step.titleColor)
-
-        description.text = step.description(requireContext())
-        description.setTextColor(step.descriptionColor)
-
-        action_1.background = button(step.buttonColor)
-        action_1.text = step.button(requireContext())
-        action_1.setTextColor(step.buttonTextColor)
-        action_1.setOnClickListener { startCoroutineAsync { next() } }*/
 
     }
 
