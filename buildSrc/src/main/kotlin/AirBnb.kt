f4ca1df740d8ede92a9761d9de7c292ec62d2af6
Giacomo Parisi <giacomo@balzo.eu>
2021-04-23 11:20:24 +0200

    Upgrade lottie abp ktx to 3.7.0

--- buildSrc/src/main/kotlin/AirBnb.kt
+++ buildSrc/src/main/kotlin/AirBnb.kt
@@ -1,4 +1,4 @@
-object AirBnb : DependencyGroup("com.airbnb.android", "3.6.1") {
+object AirBnb : DependencyGroup("com.airbnb.android", "3.7.0") {
 
     object Lottie : Dependency(AirBnb.group, "lottie", AirBnb.version)
 
