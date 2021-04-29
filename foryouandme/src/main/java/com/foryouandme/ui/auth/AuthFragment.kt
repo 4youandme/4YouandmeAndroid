eaec09f96ae57b31d9e92ffb11994881e60cf517
Giacomo Parisi <giacomo@balzo.eu>
2021-04-29 19:40:23 +0200

    Remove old auth viewmodel

--- foryouandme/src/main/java/com/foryouandme/ui/auth/AuthFragment.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/AuthFragment.kt
@@ -1,20 +1,8 @@
 package com.foryouandme.ui.auth
 
 import com.foryouandme.R
-import com.foryouandme.core.arch.android.BaseFragmentOld
-import com.foryouandme.core.arch.android.getFactory
-import com.foryouandme.core.arch.android.viewModelFactory
-import com.foryouandme.core.ext.navigator
+import com.foryouandme.core.arch.android.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AuthFragment : BaseFragmentOld<AuthViewModel>(R.layout.auth) {
-
-    override val viewModel: AuthViewModel by lazy {
-        viewModelFactory(
-            this,
-            getFactory { AuthViewModel(navigator) }
-        )
-    }
-
-}
+class AuthFragment : BaseFragment(R.layout.auth)
