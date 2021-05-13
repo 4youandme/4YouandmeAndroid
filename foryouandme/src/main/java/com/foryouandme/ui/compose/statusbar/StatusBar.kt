3d8ca3b045b5fb0ab1567ea7339b485575b1fb10
Giacomo Parisi <giacomo@balzo.eu>
2021-05-13 12:16:29 +0200

    Remove unused status bar composable

--- foryouandme/src/main/java/com/foryouandme/ui/compose/statusbar/StatusBar.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/compose/statusbar/StatusBar.kt
@@ -1,22 +1,10 @@
 package com.foryouandme.ui.compose.statusbar
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.statusBarsHeight
 import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
-
-@Composable
-fun TransparentStatusBar(
-    content: @Composable () -> Unit
-) {
-    StatusBar(color = Color.Transparent, content = content)
-}
 
 @Composable
 fun StatusBar(
@@ -37,13 +25,3 @@ fun StatusBar(
 
 private fun isDark(color: Color): Boolean =
     ((color.red * 0.299) + (color.green * 0.587) + (color.blue * 0.114)) < 186
-
-@Composable
-fun StatusBarSpacer() {
-    Spacer(
-        modifier =
-        Modifier
-            .statusBarsHeight() // Match the height of the status bar
-            .fillMaxWidth()
-    )
-}
