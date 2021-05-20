2f9a68965e41ef3a4fc83e940b87499db4507593
Giacomo Parisi <giacomo@balzo.eu>
2021-05-20 12:29:25 +0200

    Release 0.1.48

--- buildSrc/src/main/java/ProjectCofig.kt
+++ buildSrc/src/main/java/ProjectCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 30
-    const val version_code = 47
-    const val version_name = "0.1.47"
+    const val version_code = 48
+    const val version_name = "0.1.48"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
