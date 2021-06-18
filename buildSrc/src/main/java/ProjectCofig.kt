48a429a43d15e1746723307bf8c2bd9349ec105d
Giacomo Parisi <giacomo@balzo.eu>
2021-06-18 16:04:30 +0200

    Release 0.2.0

--- buildSrc/src/main/java/ProjectCofig.kt
+++ buildSrc/src/main/java/ProjectCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 30
-    const val version_code = 50
-    const val version_name = "0.1.50"
+    const val version_code = 51
+    const val version_name = "0.2.0"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
