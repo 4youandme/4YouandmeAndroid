eeafec4afef61782cf8c84f5baff87f776701602
Giacomo Parisi <giacomo@balzo.eu>
2021-07-02 16:32:17 +0200

    Release 0.2.3

--- buildSrc/src/main/java/ProjectCofig.kt
+++ buildSrc/src/main/java/ProjectCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 30
-    const val version_code = 53
-    const val version_name = "0.2.2"
+    const val version_code = 54
+    const val version_name = "0.2.3"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
